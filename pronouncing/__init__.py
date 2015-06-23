f832aecc36974523a8e2063ada465ebbd3ca1841
Allison Parrish <allison@decontextualize.com>
2015-06-23 16:01:03 -0400

    bumped version

--- pronouncing/__init__.py
+++ pronouncing/__init__.py
@@ -4,7 +4,7 @@ from pkg_resources import resource_stream
 
 __author__ = 'Allison Parrish'
 __email__ = 'allison@decontextualize.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 pronunciations = None
 
